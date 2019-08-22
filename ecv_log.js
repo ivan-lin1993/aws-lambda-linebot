5d199142edc72dabf8609842f3a2a9be2e7c8ad8
Ivan Lin <ivan.lin1993@gmail.com>
2019-08-22 12:13:31 +0800

    fix

--- ecv_log.js
+++ ecv_log.js
@@ -16,21 +16,25 @@ let get_cur_daily_logs = async (date=null) => {
         date = date.getFullYear() + "/" + ("0"+(date.getMonth()+1)).slice(-2) + "/" + ("0" + date.getDate()).slice(-2);
     }
     let option = get_option()
+    let query = "select job_status, count(*) as mcount \
+                    FROM at_job_log where job_name = 'cur billing job' and job_title like 'Daily-" + date +"%' group by job_status order by job_status" ;
     let mjson = {
         "usage":"is_query",
         "host_p":"/Prod/Billing/DB/aurora-endpoint",
         "user_p":"/Prod/Billing/DB/aurora-user",
         "password_p":"/Prod/Billing/DB/aurora-pwd",
         "DB":"atlas",
-        "query":"select id, job_name, job_title, job_start_time, job_stop_time, job_status FROM at_job_log where job_name = 'cur billing job' and job_title like 'Daily-" + date +"%' and job_status <> 2",
+        "query": query,
     }
     option['json']=mjson
     let data = await request.post('https://i3d13vuq94.execute-api.us-west-2.amazonaws.com/deploy/',option)
-    // console.log(data)
-    return data;
+    console.log(data)
+    let res = "";
+    for(let i = 0; i< data['result'].length ; i++) {
+        res += "job_status:" + data['result'][i]['job_status'] + " : " + data['result'][i]['mcount'] + "\n"
+    }    
+    return res;
 }
-
-
 
 module.exports = {
     get_cur_daily_logs: get_cur_daily_logs
